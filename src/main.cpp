61ee795579d72b68d651389c0b54b261fcc3f7f3
Christian Pfitzner <christian.pfitzner@evocortex.com>
2017-09-22 14:08:08 +0200

    -- changes to include opencv

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,45 @@
 #include <elsdetector.hpp>
 #include <elsdsvgwriter.hpp>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+
 using namespace std;
 using namespace elsd;
 
 int main(int argc, char **argv)
-    {
-    if (argc < 2)
-        cerr << argv[0] << "<image_name.pgm>" << endl;
+{
+   if (argc < 2) cerr << argv[0] << "<image_name>" << endl;
 
-    string inFile = argv[1];
-    string outFile = inFile + ".svg";
+   const string pgm_file_name = "/tmp/input.pgm";
 
-    ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
-    ShapesDetectorInterface::Ptr detector(new ElsDetector);
-    SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
+   cv::Mat input = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+   cv::imwrite(pgm_file_name, input);
 
-    detector->run(image);
-    svg->setImageSize(image->xsize(), image->ysize());
+   string inFile  = pgm_file_name;
+   string outFile = "/tmp/result.svg";
 
-    svg->addLineSegments(detector->getLineSegments().begin(), detector->getLineSegments().end());
-    svg->addArcs(detector->getCircularArcs().begin(), detector->getCircularArcs().end());
-    svg->addArcs(detector->getEllipticalArcs().begin(), detector->getEllipticalArcs().end());
 
-    ofstream ofs(outFile, ofstream::out);
-    ofs << *svg;
-    ofs.close();
+   ImageInterface::Ptr image(new ElsdPgmFileReader(inFile));
 
-    cout << inFile << endl;
-    cout << detector->getEllipticalArcs().size() << " elliptical arcs, "
-         << detector->getCircularArcs().size() << " circular arcs, "
-         << detector->getLineSegments().size() << " line segments"
-         << endl;
+   ShapesDetectorInterface::Ptr detector(new ElsDetector);
+   SvgWriterInterface::Ptr svg(new ElsdSvgWriter);
 
-    return 0;
-    }
+   detector->run(image);
+   svg->setImageSize(image->xsize(), image->ysize());
+   svg->addLineSegments(detector->getLineSegments().begin(), detector->getLineSegments().end());
+   svg->addArcs(detector->getCircularArcs().begin(),         detector->getCircularArcs().end());
+   svg->addArcs(detector->getEllipticalArcs().begin(),       detector->getEllipticalArcs().end());
+
+   ofstream ofs(outFile, ofstream::out);
+   ofs << *svg;
+   ofs.close();
+
+   cout << inFile << endl;
+   cout << detector->getEllipticalArcs().size() << " elliptical arcs, "
+        << detector->getCircularArcs().size()   << " circular arcs, "
+        << detector->getLineSegments().size()   << " line segments" << endl;
+
+   return 0;
+}
